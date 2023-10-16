2fa5dc17f227ff652f6226f7addb921e1c6a44bd
novikovka <a.novikovka@yandex.ru>
2023-10-16 10:25:56 +0500

    practice 3 update

--- 3.9 Практическая работа.js
+++ 3.9 Практическая работа.js
@@ -1,3 +1,5 @@
+//задача1
+
 let password1 = '1234-'; //пример1
 if(((password1.includes('-')) || (password1.includes('_'))) && (password1.length >= 4)){
     console.log('Пароль надежный');
@@ -25,3 +27,25 @@ if(((password4.includes('-')) || (password4.includes('_'))) && (password4.length
 } else{
     console.log('Пароль недостаточно надёжный');
 }
+
+//задача 2
+
+let userName = "ДаВИд";
+let userSurname = "пРЕоБраЖЕнСкий";
+
+let UpperStrName = userName[0].toUpperCase();
+let UpperStrSurname = userSurname[0].toUpperCase();
+
+let LowerStrName = userName.substring(1).toLowerCase();
+let LowerStrSurname = userSurname.substring(1).toLowerCase();
+
+let resultName = UpperStrName + LowerStrName;
+let resultSurename = UpperStrSurname + LowerStrSurname;
+
+console.log(resultName, resultSurename);
+console.log(resultName === userName ? 'Имя осталось без изменений' : 'Имя было преобразовано');
+
+//задача 3 
+
+let number = 13;
+console.log(number%2 === 0 ? 'Число чётное': 'Число нечётное');
