a5e6efad87bf74945f8974a8a3e32b766cc5e8eb
novikovka <a.novikovka@yandex.ru>
2023-10-30 17:15:15 +0500

    practice 9

---
+++ практика 9/main.js
@@ -0,0 +1,91 @@
+(() => {
+    let numbers = [];
+    for (let i = 1; i <=8; i+=1) { //создаем массив попарных чисел numbers
+        numbers.push(i);
+        numbers.push(i);
+    }
+
+    const shuffle = (array) => {
+        let m = array.length, t, i; 
+        while (m) {
+          i = Math.floor(Math.random() * m--);
+          t = array[m];
+          array[m] = array[i];
+          array[i] = t;
+        }   
+        return array;
+    }
+
+    let randomNumbers = shuffle(numbers);  //создаем массив перемешанных чисел randomNumbers
+    
+    let i = 0;
+    function deleteNumber(){ //функция для заполнения карточек числами из массива
+        let item = randomNumbers[i];
+        i++;
+        return item;
+    }
+
+    let arrayCards = []
+    let openedCards = [];
+    const container = document.querySelector('.container')
+    let openedCardsCount = [];
+
+    for (let i = 1; i <=16; i+=1) {
+        let card = document.createElement('div');      
+        card.classList.add('card');
+        card.textContent = '';  
+           
+        let item = {cardItem: card, cardNumber:deleteNumber()}
+        arrayCards.push(item);
+        
+        container.append(item.cardItem); 
+        
+        card.addEventListener('click', () => {
+            card.textContent = `${item.cardNumber}`; 
+            openedCards.push(item);
+    
+            function contains(arr, elem) {
+                for (var i = 0; i < arr.length; i++) {
+                    if (arr[i] === elem) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+   
+            if(openedCards.length === 2){
+                function clearValue(card1, card2){
+                    card1.textContent = '';
+                    card2.textContent = '';
+                }
+
+                if(openedCards[0].cardNumber !== openedCards[1].cardNumber){
+                    setTimeout(clearValue, 700, openedCards[0].cardItem, openedCards[1].cardItem);                  
+                }
+                else{
+                    openedCards[0].cardItem.textContent = `${item.cardNumber}`;
+                    openedCards[1].cardItem.textContent = `${item.cardNumber}`;
+                    if(!contains(openedCardsCount, openedCards[0])){
+                        openedCardsCount.push(openedCards[0]);
+                    }
+                    if(!contains(openedCardsCount, openedCards[1])){
+                        openedCardsCount.push(openedCards[1]);
+                    }                              
+                }
+                openedCards = [];
+            }
+    
+            if(openedCardsCount.length === 16){
+                const button = document.createElement('button');
+                function windowReload(){ 
+                    window.location.reload()
+                }
+                button.addEventListener('click', windowReload);
+                container.append(button);
+                button.classList.add('bth');
+                button.textContent = "Сыграть ещё раз";      
+            }
+        });      
+    } 
+})();
+
